dfce9f8e55ec81bca92b456356c9de79e132d759
yonle <fledyonle@gmail.com>
2021-03-16 18:44:53 +0800

    Fixed known issue

--- waifu.pics.js
+++ waifu.pics.js
@@ -1,65 +1,63 @@
 const fetch = require("node-fetch");
-const sfwKey = [
+const sfwCategories = [
 	"waifu", "neko", "shinobu", "megumin", "bully", "cuddle", "cry", "hug", "awoo", "kiss", "lick", "pat", "smug", "bonk", "yeet", "blush", "smile", "wave", "highfive", "handhold", "nom", "bite", "glomp", "kill", "slap", "happy", "wink", "poke", "dance", "cringe"
 ];
-const nsfwKey = [
+const nsfwCategories = [
 	"waifu", "neko", "blowjob", "trap"
 ];
 
-module.exports = function Waifu() {
-	return many("sfw/waifu");
+module.exports = function Waifu(ex) {
+	return many("sfw/waifu", ex);
 };
 
-module.exports.sfw = function getSfw(categories) {
-	return new Promise((res, rej) => {
-		if (!sfwKey.includes(categories)) return rej("Not SFW Key");
-		return get("sfw/"+categories).then(res).catch(rej);
-	});
+module.exports.sfw = async function getSfw(categories) {
+		if (!sfwCategories.includes(categories)) {
+			let err = new Error("Not SFW categories");
+                        err.code = "ERR_INVALID_CATEGORIES";
+                        return err;
+		}
+		return await get("sfw/"+categories);
 }
 
-module.exports.sfw.key = sfwKey;
-module.exports.sfw.many = function getManySfw(categories) {
-        return new Promise((res, rej) => {
-                if (!sfwKey.includes(categories)) return rej("Not SFW Key");
-                return many("sfw/"+categories).then(res).catch(rej);
-        });
+module.exports.sfw.categories = sfwCategories;
+module.exports.sfw.many = async function getManySfw(categories) {
+                if (!sfwCategories.includes(categories)) {
+			let err = new Error("Not SFW categories");
+                        err.code = "ERR_INVALID_CATEGORIES";
+                        return err;
+		}
+                return await many("sfw/"+categories);
 }
-module.exports.nsfw = function getNsfw(categories) {
-        return new Promise((res, rej) => {
-                if (!nsfwKey.includes(categories)) return rej("Not SFW Key");
-                return get("nsfw/"+categories).then(res).catch(rej);
-        });
+module.exports.nsfw = async function getNsfw(categories) {
+                if (!nsfwCategories.includes(categories)) {
+			let err = new Error("Not NSFW categories");
+                        err.code = "ERR_INVALID_CATEGORIES";
+                        return err;
+		}
+                return await get("nsfw/"+categories);
 }
 
-module.exports.nsfw.key = nsfwKey;
-module.exports.nsfw.many = function getManyNsfw(categories) {
-        return new Promise((res, rej) => {
-                if (!nsfwKey.includes(categories)) return rej("Not SFW Key");
-                return many("nsfw/"+categories).then(res).catch(rej);
-        });
+module.exports.nsfw.categories = nsfwCategories;
+module.exports.nsfw.many = async function getManyNsfw(categories) {
+                if (!nsfwCategories.includes(categories)) {
+			let err = new Error("Not NSFW categories");
+			err.code = "ERR_INVALID_CATEGORIES";
+			return err;
+		};
+                return await many("nsfw/"+categories);
 }
 
 
 async function many (endpoint, exclude) {
-	return new Promise((res, rej) => {
-		return await fetch('https://waifu.pics/api/many/'+endpoint, {
-        		method: 'post',
-	        	body:    JSON.stringify({ exclude: exclude||[] }),
-        		headers: { 'Content-Type': 'application/json' },
-		})
-		.then(res => res.json())
-		.then(res)
-		.catch(rej);
-	});
+	return await fetch('https://waifu.pics/api/many/'+endpoint, {
+       		method: 'post',
+        	body:    JSON.stringify({ exclude: exclude||[] }),
+       		headers: { 'Content-Type': 'application/json' },
+	}).then(res => res.json());
 }
 
 async function get (endpoint) {
-        return new Promise((res, rej) => {
-                return await fetch('https://waifu.pics/api/'+endpoint, {
-                        method: 'get'
-                })
-                .then(res => res.json())
-                .then(res)
-                .catch(rej);
-        });
+        return await fetch(`https://waifu.pics/api/${endpoint}`).then(res => res.json())
 }
+
+
